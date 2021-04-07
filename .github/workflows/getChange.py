573900ce1ec9c06f4dfd7463d9d9cd8925ed9dfd
xichen1 <xichen.pan@gmail.com>
2021-04-06 18:29:39 -0600

    fix

--- .github/workflows/getChange.py
+++ .github/workflows/getChange.py
@@ -7,15 +7,16 @@ def main():
     print(sys.argv)
     result = []
     # example: jdk/src/windows/classes/java/lang/
-    regex = "/jdk/src/([a-zA-Z0-9 ]+?)/classes/java/([a-zA-Z0-9 ]+?)/"
+    pattern = re.compile(
+        r"jdk/src/([a-zA-Z0-9 ]+?)/classes/java/([a-zA-Z0-9 ]+?)/")
     # TODO: handle the change of buildenv dir
     for argument in sys.argv:
-        if len(re.findall(regex, argument)) > 0:
-            result.append(re.findall(regex, argument))
+        if len(re.match()(pattern, argument)) > 0:
+            result.append(argument)
 
     if not result:
         result.append('skip')
-    print('::set-output name=build_lists::{}'.format(json.dumps(result)))
+    print('::set-output name=locations::{}'.format(json.dumps(result)))
 
 
 if __name__ == "__main__":
