dc91eb3deb19217866e54fcef1634482533f26cc
xichen1 <xichen.pan@gmail.com>
2021-04-06 18:03:46 -0600

    fix

--- jdk/src/windows/classes/java/lang/ClassLoaderHelper.java
+++ jdk/src/windows/classes/java/lang/ClassLoaderHelper.java
@@ -28,16 +28,17 @@ import java.io.File;
 
 class ClassLoaderHelper {
 
-    private ClassLoaderHelper() {}
+    private ClassLoaderHelper() {
+    }
 
     /**
-     * Returns an alternate path name for the given file
-     * such that if the original pathname did not exist, then the
-     * file may be located at the alternate location.
-     * For most platforms, this behavior is not supported and returns null.
+     * Returns an alternate path name for the given file such that if the original
+     * pathname did not exist, then the file may be located at the alternate
+     * location. For most platforms, this behavior is not supported and returns
+     * null.
      */
     static File mapAlternativeName(File lib) {
         return null;
-        
+
     }
 }
