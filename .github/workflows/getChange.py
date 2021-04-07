660003946ec11ce3e97c6c85d9b2b15b480bd222
xichen1 <xichen.pan@gmail.com>
2021-04-06 18:35:04 -0600

    fix

--- .github/workflows/getChange.py
+++ .github/workflows/getChange.py
@@ -11,7 +11,7 @@ def main():
         r"jdk/src/([a-zA-Z0-9 ]+?)/classes/java/([a-zA-Z0-9 ]+?)/")
     # TODO: handle the change of buildenv dir
     for argument in sys.argv:
-        if len(re.match(pattern, argument)) > 0:
+        if (re.match(pattern, argument)):
             result.append(argument)
 
     if not result:
